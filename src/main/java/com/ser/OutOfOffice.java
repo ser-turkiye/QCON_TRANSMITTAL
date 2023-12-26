403d65d560db7c4995c10c4f0efb29120565022e
yunusemregmail <yunusemreyildiz@gmail.com>
2023-12-26 14:16:27 +0300

    send2Dcc ve outOfOffice EKLENDI

---
+++ src/main/java/com/ser/OutOfOffice.java
@@ -0,0 +1,35 @@
+package com.ser;
+
+import com.ser.blueline.*;
+import com.ser.blueline.bpm.ITask;
+import com.ser.blueline.bpm.ITaskDefinition;
+import com.ser.evITAWeb.EvitaWebException;
+import com.ser.evITAWeb.api.IDialog;
+import com.ser.evITAWeb.api.actions.IBasicAction;
+import com.ser.evITAWeb.api.actions.IMessageAction;
+import com.ser.evITAWeb.api.actions.IStopFurtherAction;
+import com.ser.evITAWeb.api.controls.IControl;
+import com.ser.evITAWeb.api.controls.ITextField;
+import com.ser.evITAWeb.scripting.Doxis4ClassFactory;
+import com.ser.evITAWeb.scripting.bpmservice.task.TaskScripting;
+import org.slf4j.Logger;
+import utils.Utils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Vector;
+
+public class OutOfOffice extends TaskScripting {
+
+
+    private static Logger log;
+    public ISession ses;
+    private IDialog dlg;
+    public OutOfOffice(ITask task){
+        super(task);
+        this.log=super.log;
+    }
+
+
+}
