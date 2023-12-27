be8eb7b1ee4f05d72fc2eeba7719a62f6e499131
oeroglu <oeroglu@gmail.com>
2023-12-27 13:31:21 +0300

    send-to-dcc init validation

--- src/main/java/utils/GeneralLib.java
+++ src/main/java/utils/GeneralLib.java
@@ -5,9 +5,37 @@ import com.ser.blueline.metaDataComponents.IStringMatrix;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 public class GeneralLib {
+
+    public static class ClassIDs{
+        public static final String ProjectWorkspace = "32e74338-d268-484d-99b0-f90187240549";
+        public static final String Contact = "d7ffea9d-3419-4922-8ffa-a0310add5723";
+        public static final String EngineeringDocument = "3b3078f8-c0d0-4830-b963-88bebe1c1462";
+    }
+    public static class Databases{
+        public static final String ProjectWorkspace = "PRJ_FOLDER";
+        public static final String SupplierContact = "BPWS";
+    }
+    public static class Descriptors{
+        public static final String ProjectNo = "ccmPRJCard_code";
+        public static final String ProjectName = "ccmPRJCard_name";
+        public static final String DocStatus = "ccmPrjDocStatus";
+        public static final String EMail = "PrimaryEMail";
+        public static final String FileName = "ccmPrjDocFileName";
+    }
+    public static class CheckValues{
+        public static final List<String> SendDocStatuses = new ArrayList<>(Arrays.asList(
+                "",
+                "10",
+                "20"
+        ));
+
+    }
     public static ISession ses;
     private static final Logger log = LoggerFactory.getLogger(GeneralLib.class);
     public static void setSes(ISession ses) {
@@ -16,32 +44,26 @@ public class GeneralLib {
 
     public static IDocument getProjectCard(ISession s, String pCode)  {
         setSes(s);
-
-        String objectID ="32e74338-d268-484d-99b0-f90187240549" ;
-
         StringBuilder builder = new StringBuilder();
-        builder.append("TYPE = '").append(objectID).append("'")
+        builder.append("TYPE = '").append(ClassIDs.ProjectWorkspace).append("'")
                 .append(" AND ")
-                .append("ccmPRJCard_code").append(" = '").append(pCode).append("'");
+                .append(Descriptors.ProjectNo).append(" = '").append(pCode).append("'");
         String whereClause = builder.toString();
-        log.warn("Where Clause: " + whereClause);
+        System.out.println("Where Clause: " + whereClause);
 
-        IInformationObject[] informationObjects = createQuery(s, new String[] {"PRJ_FOLDER"} , whereClause , 1);
+        IInformationObject[] informationObjects = createQuery(s, new String[] {Databases.ProjectWorkspace} , whereClause , 1);
         if(informationObjects.length < 1) {return null;}
         return (IDocument) informationObjects[0];
     }
     public static IDocument getContactRecord(ISession ses, String eMail)  {
-        setSes(ses);
-        String SupplierContactWS = "d7ffea9d-3419-4922-8ffa-a0310add5723" ;
-
         StringBuilder builder = new StringBuilder();
-        builder.append("TYPE = '").append(SupplierContactWS).append("'")
+        builder.append("TYPE = '").append(ClassIDs.Contact).append("'")
                 .append(" AND ")
-                .append("PrimaryEMail").append(" = '").append(eMail).append("'");
+                .append(Descriptors.EMail).append(" = '").append(eMail).append("'");
         String whereClause = builder.toString();
-        log.info("Where Clause: " + whereClause);
+        System.out.println("Where Clause: " + whereClause);
 
-        IInformationObject[] informationObjects = createQuery(ses, new String[] {"BPWS"} , whereClause , 1);
+        IInformationObject[] informationObjects = createQuery(ses, new String[] {Databases.SupplierContact} , whereClause , 1);
         if(informationObjects.length < 1) {return null;}
         return (IDocument) informationObjects[0];
     }
